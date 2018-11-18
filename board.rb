55254b1383c8f95d7189247aeb973278c9f819d9
jkulton <jonkulton@gmail.com>
2018-11-18 17:06:12 -0500

    Refactor DB code into db-related class

--- board.rb
+++ board.rb
@@ -1,9 +1,9 @@
 require "sinatra"
 require "sinatra/content_for"
 require "tilt/erubis"
-require "pg"
 require "date"
 require "redcarpet"
+require_relative "board_db"
 
 configure do
   enable :sessions
@@ -13,77 +13,81 @@ end
 
 configure(:development) do
   require "sinatra/reloader"
+  also_reload "board_db.rb"
 end
 
 helpers do
 end
 
 before do
-  @db = if Sinatra::Base.production?
-    PG.connect(ENV['DATABASE_URL'])
-  else
-    PG.connect(dbname: 'tinyboard')
-  end
-
+  @database = BoardDB.new
   session ||= {dark_mode: false}
 end
 
 after do
-  @db.close
+  @database.disconnect
 end
 
-# Homepage
 get "/" do
   @user = session[:user]
-
-  sql = <<~SQL
-    SELECT DISTINCT topics.*,
-     (SELECT COUNT(messages.id) FROM messages WHERE topic_id = topics.id) AS "message_count",
-     (SELECT author_initials FROM messages WHERE topic_id = topics.id ORDER BY posted LIMIT 1) AS "author_initials",
-     (SELECT author_theme FROM messages WHERE topic_id = topics.id ORDER BY posted LIMIT 1) AS "author_theme",
-     (SELECT posted FROM messages WHERE topic_id = topics.id ORDER BY posted DESC LIMIT 1) AS "last_message"
-    FROM topics
-    INNER JOIN messages
-    ON topics.id = messages.topic_id
-    ORDER BY last_message DESC
-    LIMIT 50;
-  SQL
-
-  topics_result = @db.exec(sql);
-
-  @topics = topics_result.map do |tuple|
-    { id: tuple["id"],
-      title: tuple["title"],
-      author_initials: tuple["author_initials"],
-      author_theme: tuple["author_theme"],
-      message_count: tuple["message_count"] }
-  end
-
+  @topics = @database.recent_topics
   erb :home
 end
 
-# View a topic
+get "/join" do
+  if session[:user]
+    session[:error] = "You've already joined!"
+    redirect "/"
+  end
+
+  erb :join
+end
+
+post "/join" do
+  @user = session[:user]
+  initials = params["initials"].strip.upcase
+
+  if /^[A-Z]{2}$/ =~ initials
+    session[:user] = { initials: initials, theme: params["theme"] }
+    redirect "/"
+  else
+    session[:error] = 'Initials must be two alphabetical characters.'
+    erb :join
+  end
+end
+
+get "/topic/new" do
+  @user = session[:user]
+
+  if !@user
+    session[:error] = 'You must join to make a topic!'
+    redirect "/"
+  end
+
+  erb :new_topic
+end
+
+post "/topic/new" do
+  @user = session[:user]
+  topic_title = params["title"].strip
+  content = params["content"].strip
+
+  if content.empty? || topic_title.empty?
+    session[:error] = "Title and message must not be blank!"
+    erb :new_topic
+  else
+    topic_id = @database.add_topic(topic_title, content, @user[:initials], @user[:theme])
+    redirect "/topic/#{topic_id}"
+  end
+end
+
 get "/topic/:id" do
   @user = session[:user]
   @topic_id = params["id"]
-  sql = 'SELECT * FROM topics INNER JOIN messages ON topics.id = messages.topic_id WHERE topic_id = $1 ORDER BY posted;'
-  messages_result = @db.exec_params(sql, [@topic_id]);
-
-  @title = messages_result.first["title"]
-  @messages = messages_result.map do |tuple|
-    formatted_date = DateTime.parse(tuple["posted"]).strftime("%B %d, %Y @ %I:%M%p")
-    formatted_content = Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true).render(tuple["content"])
-
-    { content: formatted_content,
-      author_initials: tuple["author_initials"],
-      author_theme: tuple["author_theme"],
-      posted: formatted_date }
-  end
-
+  @topic = @database.topic(@topic_id)
   erb :topic
 end
 
-# POST for adding a message to a topic
 post "/topic/:id" do
   @user = session[:user]
   content = params["content"].strip
@@ -95,70 +99,13 @@ post "/topic/:id" do
     session[:error] = "Message must not be blank!"
     redirect "/topic/#{topic_id}"
   else
-    sql = 'INSERT INTO messages (topic_id, content, author_initials, author_theme) VALUES ($1, $2, $3, $4)'
-    @db.exec_params(sql, [topic_id, content, author_initials, author_theme])
+    @database.add_message(topic_id, content, author_initials, author_theme)
     redirect "/topic/#{topic_id}"
   end
 end
 
-# POST for setting user options (currently only dark/light mode)
 post "/options" do
   @user = session[:user]
   session[:dark_mode] = (params["darkMode"] == "true")
   redirect "/"
-end
-
-# Display join form
-get "/join" do
-  if session[:user]
-    session[:error] = "You've already joined!"
-    redirect "/"
-  end
-
-  erb :join
-end
-
-# Handle join form
-post "/join" do
-  @user = session[:user]
-  initials = params["initials"].strip.upcase
-
-  if /^[A-Z]{2}$/ =~ initials
-    theme = params["theme"]
-    session[:user] = { initials: initials, theme: theme }
-    redirect "/"
-  else
-    session[:error] = 'Initials must be two alphabetical characters.'
-    erb :join
-  end
-end
-
-# Display new topic form
-get "/new-topic" do
-  @user = session[:user]
-  redirect "/" if !session[:user]
-  erb :new_topic
-end
-
-# Handle new topic form
-post "/topics/new" do
-  @user = session[:user]
-  topic_title = params["title"].strip
-  content = params["content"].strip
-  author_theme = session[:user][:theme]
-  author_initials = session[:user][:initials]
-
-  if content.empty? || topic_title.empty?
-    session[:error] = "Title and message must not be blank!"
-    erb :new_topic
-  else
-    topics_sql = 'INSERT INTO topics (title) VALUES ($1) RETURNING id;'
-    insert_result = @db.exec_params(topics_sql, [topic_title])
-    topic_id = insert_result.first["id"]
-
-    message_sql = 'INSERT INTO messages (topic_id, content, author_initials, author_theme) VALUES ($1, $2, $3, $4)'
-    @db.exec_params(message_sql, [topic_id, content, author_initials, author_theme])
-
-    redirect "/"
-  end
 end
---
+++ board_db.rb
@@ -0,0 +1,73 @@
+require "pg"
+
+class BoardDB
+  def initialize
+    @db = if Sinatra::Base.production?
+      PG.connect(ENV['DATABASE_URL'])
+    else
+      PG.connect(dbname: 'tinyboard')
+    end
+  end
+
+  def recent_topics
+    sql = <<~SQL
+      SELECT DISTINCT topics.*,
+       (SELECT COUNT(messages.id) FROM messages WHERE topic_id = topics.id) AS "message_count",
+       (SELECT author_initials FROM messages WHERE topic_id = topics.id ORDER BY posted LIMIT 1) AS "author_initials",
+       (SELECT author_theme FROM messages WHERE topic_id = topics.id ORDER BY posted LIMIT 1) AS "author_theme",
+       (SELECT posted FROM messages WHERE topic_id = topics.id ORDER BY posted DESC LIMIT 1) AS "last_message"
+      FROM topics
+      INNER JOIN messages
+      ON topics.id = messages.topic_id
+      ORDER BY last_message DESC
+      LIMIT 50;
+    SQL
+
+    topics_result = @db.exec(sql);
+
+    topics_result.map do |tuple|
+      { id: tuple["id"],
+        title: tuple["title"],
+        author_initials: tuple["author_initials"],
+        author_theme: tuple["author_theme"],
+        message_count: tuple["message_count"] }
+    end
+  end
+
+  def topic(id)
+    sql = 'SELECT * FROM topics INNER JOIN messages ON topics.id = messages.topic_id WHERE topic_id = $1 ORDER BY posted;'
+    messages_result = @db.exec_params(sql, [id]);
+
+    title = messages_result.first["title"]
+    messages = messages_result.map do |tuple|
+      formatted_date = DateTime.parse(tuple["posted"]).strftime("%B %d, %Y @ %I:%M%p")
+      formatted_content = Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true).render(tuple["content"])
+
+      { content: formatted_content,
+        author_initials: tuple["author_initials"],
+        author_theme: tuple["author_theme"],
+        posted: formatted_date }
+    end
+
+    {title: title, messages: messages}
+  end
+
+  def add_message(topic_id, content, author_initials, author_theme)
+    sql = 'INSERT INTO messages (topic_id, content, author_initials, author_theme) VALUES ($1, $2, $3, $4)'
+    @db.exec_params(sql, [topic_id, content, author_initials, author_theme])
+  end
+
+  def add_topic(title, content, author_initials, author_theme)
+    topics_sql = 'INSERT INTO topics (title) VALUES ($1) RETURNING id;'
+    insert_result = @db.exec_params(topics_sql, [title])
+    topic_id = insert_result.first["id"]
+
+    add_message(topic_id, content, author_initials, author_theme)
+
+    topic_id
+  end
+
+  def disconnect
+    @db.close
+  end
+end
