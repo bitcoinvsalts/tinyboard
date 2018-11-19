74231ce60664c3a1d823936ac4d09fcced445e8f
jkulton <jonkulton@gmail.com>
2018-11-19 09:29:31 -0500

    Add check constraint for author_initials column

--- db/schema.sql
+++ db/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE messages (
   id serial PRIMARY KEY,
   topic_id integer REFERENCES topics (id) NOT NULL,
   content text NOT NULL,
-  author_initials char(2) NOT NULL,
+  author_initials char(2) NOT NULL CHECK (author_initials ~ '^[A-Z]{2}$'),
   author_theme color NOT NULL,
   posted timestamp NOT NULL DEFAULT NOW()
 );
